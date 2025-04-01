3552dc628a9a67586eb8fc15eae2f826fa2c97f7
advanced-go <mark.wilson.go@outlook.com>
2025-04-01 12:56:16 -0500

    Update exchange.go

--- routing/routingtest/exchange.go
+++ routing/routingtest/exchange.go
@@ -5,13 +5,31 @@ import (
 	"github.com/behavioral-ai/core/httpx"
 	"github.com/behavioral-ai/core/iox"
 	"net/http"
+	"strings"
+)
+
+const (
+	googlePath = "/google/search"
+	yahooPath  = "/yahoo/search"
 )
 
 // Exchange - HTTP exchange function
 func Exchange(r *http.Request) (resp *http.Response, err error) {
+	uri := ""
+	values := r.URL.Query()
+	q := values.Encode()
+	if strings.HasPrefix(r.URL.Path, googlePath) {
+		uri = "https://www.google.com/search?" + q
+	} else {
+		if strings.HasPrefix(r.URL.Path, yahooPath) {
+			uri = "https://search.yahoo.com/search?" + q
+		} else {
+			return httpx.NewResponse(http.StatusBadRequest, nil, nil), err
+		}
+	}
 	h := make(http.Header)
 	h.Add(iox.AcceptEncoding, iox.GzipEncoding)
-	req, _ := http.NewRequest(http.MethodGet, "https://www.google.com/search?q=golang", nil)
+	req, _ := http.NewRequest(http.MethodGet, uri, nil)
 	req.Header = h
 	resp, err = httpx.Do(req)
 	if err != nil {
