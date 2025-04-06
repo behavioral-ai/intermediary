8aeaced51062fdc44bd8e24e5e736401286871fd
advanced-go <mark.wilson.go@outlook.com>
2025-04-06 11:43:30 -0500

    Create package.go

---
+++ urn/package.go
@@ -0,0 +1,11 @@
+package urn
+
+import (
+	"github.com/behavioral-ai/intermediary/cache"
+	"github.com/behavioral-ai/intermediary/routing"
+)
+
+const (
+	CacheAgent   = cache.NamespaceName
+	RoutingAgent = routing.NamespaceName
+)
